a1ba23f0abd65ce0112c94c34036fa814c535fb4
xrsQugi <02.04.5.art@gmail.com>
2023-06-11 20:00:01 +0300

    fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,15 +12,15 @@ export default function Profile({ username="Someone", tag="Someone`s tag", locat
       </div>
       <ul className="stats">
         <li>
-          <span className="label">Followers: </span>
+          <span className="label">Followers:</span>
           <span className="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span className="label">Views: </span>
+          <span className="label">Views:</span>
           <span className="quantity">{stats.views}</span>
         </li>
         <li>
-          <span className="label">Likes: </span>
+          <span className="label">Likes:</span>
           <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
